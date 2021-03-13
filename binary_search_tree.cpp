94c249816730c5723a8fdb3649918dc18d5e2779
llee628 <gn01672397@gmail.com>
2021-03-13 15:45:22 -0500

    finished delete in BST

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 
 using namespace std;
 
@@ -26,17 +27,46 @@ class BST{
 private:
 	BSTNode* root;
 	BSTNode* Leftmost(BSTNode* current);
-	BSTNode* Successor(BSTNode* current);
+	BSTNode* inorder_Successor(BSTNode* current);
 
 public:
 	BST():root(0){};
 
 	BSTNode* search(int key);
 	void insert(int key);
+	void Delete(int key);
 	void inorderPrint();
 	void Levelorder();
 
 };
+
+BSTNode* BST::Leftmost(BSTNode* current){
+	if (current == NULL){
+		return current;
+	}
+	while (current->left != NULL){
+		current = current->left;
+	}
+	return current;
+}
+
+BSTNode* BST::inorder_Successor(BSTNode* current){
+	if (current == NULL){
+		return current;
+	}
+
+	if (current->right != NULL){
+		return Leftmost(current->right);
+	}
+
+	// for the case that right child is NULL
+	BSTNode* successor = current->parent;
+	while(successor != NULL && current == successor->right){
+		current = successor;
+		successor = successor->parent;
+	}
+	return successor;
+}
 
 BSTNode* BST::search(int key){
 	BSTNode* current = root;
@@ -57,6 +87,7 @@ BSTNode* BST::search(int key){
 }
 
 void BST::insert(int key){
+	//cout<<"i am in"<<endl;
 	BSTNode* current = root;
 	BSTNode* current_parent = NULL;
 	BSTNode* insert_node = new BSTNode(key);
@@ -67,23 +98,175 @@ void BST::insert(int key){
 		if (insert_node->key < current->key){
 			current = current->left;
 		}
-		else{
+		else if(insert_node->key > current->key){
 			current = current->right;
 		}
-
-		insert_node->parent = current_parent;
-
-		if (current->parent == NULL){
-			this->root = insert_node; //for the case that a BST is empty before insert
+		else{
+			return;
 		}
-		else if (insert_node->key < current_parent->key){
-			current_parent->left = insert_node;
+	}
+
+	insert_node->parent = current_parent;
+
+	if (current_parent == NULL){
+		this->root = insert_node; //for the case that a BST is empty before insert
+	}
+	else if (insert_node->key < current_parent->key){
+		current_parent->left = insert_node;
+	}
+	else{
+		current_parent->right = insert_node;
+	}
+}
+
+void BST::Delete(int key){
+
+	//Still need approve
+	//now can not deal with the case that when root has no child or one child
+
+	BSTNode* delete_node = search(key);
+	//check if key is in the BST
+	if (delete_node == NULL){
+		cout<<"data not found."<<endl;
+		return;
+	}
+
+	//case1: leaf
+	if (delete_node->left == NULL && delete_node->right == NULL){
+		BSTNode* parent = delete_node->parent;
+		if (delete_node == parent->left){
+			parent->left = NULL;
+			return;
 		}
 		else{
-			current_parent->right = insert_node;
+			parent->right = NULL;
+			return;
+		}
+	}
+
+	//case2: one child
+	if (delete_node->left == NULL || delete_node->right == NULL){
+		BSTNode* parent = delete_node->parent;
+		if (delete_node == parent->left){
+			if (delete_node->left != NULL){
+				parent->left = delete_node->left;
+				delete_node->left->parent = parent;
+				return;
+			}
+			else{
+				parent->left = delete_node->right;
+				delete_node->right->parent = parent;
+				return;
+			}
+		}
+		else{
+			if (delete_node->left != NULL){
+				parent->right = delete_node->left;
+				delete_node->left->parent = parent;
+				return;
+			}
+			else{
+				parent->right = delete_node->right;
+				delete_node->right->parent = parent;
+				return;
+			}
+		}
+	}
+
+	//case 3: two children
+	if (delete_node->left != NULL && delete_node->right != NULL){
+		BSTNode* successor = inorder_Successor(delete_node);
+		BSTNode* parent = successor->parent;
+
+		delete_node->key = successor->key;
+		if (parent->left == successor){
+			parent->left = NULL;
+			return;
+		}
+		else{
+			parent->right = NULL;
+			return;
 		}
 
 	}
+}
+
+void BST::inorderPrint(){
+	//cout<<"i am in"<<endl;
+
+	if (root == NULL){
+		//cout<<"i am in"<<endl;
+		return;
+	}
+
+	BSTNode* current = Leftmost(root);
+	while (current != NULL){
+		cout<<current->key<<" ";
+		current = inorder_Successor(current);
+	}
+}
+
+void BST::Levelorder(){
+	if (this->root == NULL){
+		return;
+	}
+
+	queue<BSTNode* > q;
+	q.push(this->root);
+
+	while(!q.empty()){
+		BSTNode* current = q.front();
+		q.pop();
+		cout<<current->key<<" ";
+
+		if (current->left != NULL){
+			q.push(current->left);
+		}
+
+		if (current->right != NULL){
+			q.push(current->right);
+		}
+
+	}
+}
+
+
+
+int main(){
+	BST T1;
+
+	T1.insert(8);
+	T1.insert(1000);
+	T1.insert(2);
+	T1.insert(513);
+
+	cout<<"Inorder:"<<endl;
+	T1.inorderPrint();
+	cout<<endl;
+	cout<<"Levelorder:"<<endl;
+	T1.Levelorder();
+	cout<<endl;
+
+	T1.Delete(8);
+	cout<<"Inorder:"<<endl;
+	T1.inorderPrint();
+	cout<<endl;
+	cout<<"Levelorder:"<<endl;
+	T1.Levelorder();
+	cout<<endl;
+
+	T1.Delete(512);
+	T1.Delete(513);
+	T1.Delete(1000); //run time error
+	cout<<"Inorder:"<<endl;
+	T1.inorderPrint();
+	cout<<endl;
+	cout<<"Levelorder:"<<endl;
+	T1.Levelorder();
+	cout<<endl;
+
+
+	return 0;
 
 }
 
