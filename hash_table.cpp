8f25c927b98f395c98a189e4313df6549df2f503
llee628 <gn01672397@gmail.com>
2021-05-21 17:48:19 +0800

    finished Insert and Search in hash_table

--- hash_table.cpp
+++ hash_table.cpp
@@ -40,3 +40,49 @@ public:
 
 
 };
+
+int HashChain::PreHashing(string key_str){
+    int base = 9;    //choose randomly
+    int key_int = 0;
+    int p = 1;
+
+    for (int i = (int)key_str.size()-1; i>=0; i-=1){
+        key_int += key_str[i]*p;
+        p *= base;
+
+    }
+
+    return key_int;
+}
+
+int HashChain::HashFunction(string key_str){
+    int key_int = PreHashing(key_str);
+    int hashcode = key_int % this->size;
+}
+
+void HashChain::Insert(dict data){
+    /*
+     * 1. get index from hashfunction
+     * 2. insert data at the front of linked list
+    **/
+
+   int index = HashFunction(data.key);
+   table[index].push_front(data);
+}
+
+string HashChain::Search(string key){
+    /*
+     * 1. get index from hashfunction
+     * 2. traversal in linked list
+    **/
+
+   int index = HashFunction(key);
+   for (list<dict>::iterator itr = table[index].begin(); itr != table[index].end(); itr+=1){
+       if ((*itr).key == key){
+           return (*itr).value;
+       }
+   }
+
+   return "...\nno such data";
+
+}
