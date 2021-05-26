e71f9602f31764461c99af126b0124418d13a2e6
llee628 <gn01672397@gmail.com>
2021-05-26 11:42:33 +0800

    Completed private methods

--- hash_table.cpp
+++ hash_table.cpp
@@ -46,3 +46,62 @@ public:
     void DisplayTable();
 
 };
+
+int HashChainNode::HashFunction(int key){
+    //Multiplication method
+    double A = 0.6180339887;     //golden ratio
+    double frac = key*A - floor(key*A);
+
+    return floor(this->size*frac);
+}
+
+void HashChainNode::Rehashing(int size_orig){
+
+    Node** newtable = new Node* [size]; // allocate memory for new table
+    for (int i = 0; i < size; i++){     // initialization
+        newtable[i] = 0;                // ensure every node in slots points to NULL
+    }
+
+    for (int i = 0; i < size_orig; i++){ //visit every node in the original table
+
+        Node* curr_orig = table[i];
+        Node* right_orig = NULL;    // on the right of current
+
+        while(curr_orig != NULL){
+            right_orig = curr_orig->next;
+
+            int index = HashFunction(curr_orig->key);   // get index of slot in new table
+
+            //push_front
+            if (newtable[index] == NULL){   // if newtable[index] is empty
+                newtable[index] = curr_orig;
+                newtable[index]->next = 0;
+
+            }
+            else{   // if newtable[index] is not empty
+                Node* next = newtable[index];
+                newtable[index] = curr_orig;
+                newtable[index]->next = next;
+
+            }
+            curr_orig = right_orig;
+        }
+
+    }
+    delete [] table; // release memory of original table
+    this->table = newtable; // point table of object to new table
+
+}
+
+void HashChainNode::TableDoubling(){
+    int size_orig = size;
+    size *= 2;              // size update
+    Rehashing(size_orig);   // create new table with larger size
+
+}
+
+void HashChainNode::TableShrinking(){
+    int size_orig = size;
+    size /= 2;              // size update
+    Rehashing(size_orig);   // create new table with smaller size
+}
