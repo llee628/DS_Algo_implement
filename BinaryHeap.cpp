efe4a8c3774e0bade388b626931003df70bab031
llee628 <gn01672397@gmail.com>
2021-05-28 20:07:23 +0800

    finshed MinHeapify and BuildMinHeap

--- BinaryHeap.cpp
+++ BinaryHeap.cpp
@@ -38,3 +38,38 @@ public:
 
 
 };
+
+void BinaryHeap::MinHeapify(int node, int length){
+    //sink
+
+    int left = 2*node;          // left child
+    int right = 2*node + 1;     // right child
+    int smallest;
+
+    if (left <= length && heap[left].key < heap[node].key){
+        smallest = left;
+    }
+    else{
+        smallest = node;
+    }
+
+    if (right <= length && heap[right].key < heap[smallest].key){
+        smallest = right;
+    }
+
+    if (smallest != node){
+        swap(heap[smallest], heap[node]);
+        MinHeapify(smallest, length);   // Minheapify the new subtree
+    }
+}
+
+void BinaryHeap::BuildMinHeap(vector<int> array){
+    // put the information of array into heap [], and leave the heap[0]
+    for (int i = 0; i < array.size(); i++){
+        heap[i+1].element = i;
+        heap[i+1].key = array[i];
+    }
+    for (int i = (int)heap.size()/2; i >= 1; i--){
+        MinHeapify(i, (int)heap.size()-1); // -1 is because we leave heap[0] blank
+    }
+}
