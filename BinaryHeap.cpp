35f8fc71b0503e27d677f3d9a3508f9336d7ca86
llee628 <gn01672397@gmail.com>
2021-05-30 16:16:40 +0800

    Finished BinaryHeap aka min-priority queue

--- BinaryHeap.cpp
+++ BinaryHeap.cpp
@@ -73,3 +73,65 @@ void BinaryHeap::BuildMinHeap(vector<int> array){
         MinHeapify(i, (int)heap.size()-1); // -1 is because we leave heap[0] blank
     }
 }
+
+void BinaryHeap::swap(struct HeapNode& p1, struct HeapNode& p2){
+
+    struct HeapNode temp = p1;
+    p1 = p2;
+    p2 = temp;
+}
+
+int BinaryHeap::FindPosition(int node){
+
+    int idx = 0;
+    for (int i = 1; i < heap.size(); i++){
+        if (heap[i].element == node){
+            idx = i;
+        }
+    }
+    
+    return idx;
+}
+
+int BinaryHeap::Minimum(){
+    return heap[1].element;
+}
+
+int BinaryHeap::ExtractMin(){
+
+    if (IsHeapEmpty()){
+        cout<<"error: heap is empty\n";
+        exit(-1);
+    }
+    int min = heap[1].element;
+
+    //delete the first element
+    heap[1] = heap[heap.size()-1]; // replace first element with last element
+    heap.erase(heap.begin() + heap.size()-1); // release the last element
+    MinHeapify(1, (int)heap.size());
+
+    return min;
+}
+
+void BinaryHeap::DecreaseKey(int node, int newKey){
+
+    int index_node = FindPosition(node);
+
+    if (newKey > heap[index_node].key){
+        cout<<"new key is larger than current key\n";
+        return;
+    }
+    heap[index_node].key = newKey;
+
+    //sift up
+    while (index_node > 1 && heap[GetParentNode(index_node)].key > heap[index_node].key){
+        swap(heap[index_node], heap[GetParentNode(index_node)]);
+        index_node = GetParentNode(index_node);
+    }
+}
+
+void BinaryHeap::MinHeapInsert(int node, int key){
+
+    heap.push_back(HeapNode(node,key));
+    DecreaseKey(node, key);  //siftup if necessary
+}
